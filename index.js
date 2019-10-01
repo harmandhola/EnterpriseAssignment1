1ff2b79544c61083f9a64bd1fab4cdfc9ea4f028
harman <harman1996dhola@icloud.com>
2019-10-01 19:41:04 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var SERVER_NAME = 'product-api'
+var PORT = 3009;
+var HOST = '127.0.0.1';
+
+var restify = require('restify');
+
+var save = require('save')
+var productsSave = save('products')
+
+var server = restify.createServer();
+
+// configure request body parser
+server.use(restify.plugins.bodyParser({ mapParams: false }));
+
+// starting server
+server.listen(3009, function() {
+  console.log('%s listening at %s', server.name, server.url);
+
+});
+
+// GET request
+server.get('/products', getAllProducts);
+
+// POST request
+server.post('/products', addNewProduct);
+
+
+// callback function mapped to GET request
+function getAllProducts(req, res, next) {
+    productsSave.find({},null, function(err, foundProducts){
+        // send 200 HTTP response code and array of found products
+        res.send(200, foundProducts);
+        next();
+    })
+}
+// callback function mapped to POST request
+function addNewProduct(req, res, next) {
+    // json payload of the request
+    var newProduct = req.body;
+
+    productsSave.create(newProduct, function(err, product){
+        // send 201 HTTP response code and created product object
+        res.send(201, product);
+        next();
+    })
+}
