19041af69805754cd9ffb91732cc8c49f84595a9
harman <harman1996dhola@icloud.com>
2019-10-06 16:26:56 -0400

    log requests ingo

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var save = require('save')
 
 var productsSave = save('products')
 
-
+var requestCounterGet = 0;
+var requestCounterPost = 0;
 
 var server = restify.createServer();
 
@@ -18,8 +19,9 @@ server.use(restify.plugins.bodyParser({ mapParams: false }));
 // starting server
 server.listen(3009, function() {
   console.log('%s listening at %s', server.name, server.url);
-
+console.log('Endpoints:',server.get,server.post);
 });
+
 
 // GET request
 server.get('/products', getAllProducts);
@@ -32,26 +34,43 @@ server.del('/products', deleteAllProducts);
 
 // callback function mapped to GET request
 function getAllProducts(req, res, next) {
+      requestCounterGet++;
+    console.log("   Request counter: " +  requestCounterGet);
+    console.log("sending request")
     productsSave.find({},null, function(err, foundProducts){
-        
+        var requestCounterString = "======> Request Count: " + requestCounterGet + "\n======================================";
         // send 200 HTTP response code and array of found products
         res.send(200, foundProducts);
+        console.log("received request");
         next();
     })
 }
 // callback function mapped to POST request
 function addNewProduct(req, res, next) {
-
+        requestCounterPost++;
+        console.log("   Request counter: " +  requestCounterPost);
+    console.log("sending request")
     // json payload of the request
     var newProduct = req.body;
 
     productsSave.create(newProduct, function(err, product){
-        console.log("Creating new products")
+        var requestCounterString = "======> Request Count: " + requestCounterPost + "\n======================================";
+        console.log("Created new products")
         // send 201 HTTP response code and created product object
         res.send(201, product);
         next();
     })
 }
 
-
+// callback function mapped to DELETE request
+function deleteAllProducts(req, res, next) {
+        console.log("Deleting all products");
+        productsSave.deleteMany({}, function(err, Products){
+            console.log("error"+ err);
+    console.log("Deleted products")
+            // send 200 HTTP response code 
+            res.send(200, Products);
+            next();
+        })
+    }
     
